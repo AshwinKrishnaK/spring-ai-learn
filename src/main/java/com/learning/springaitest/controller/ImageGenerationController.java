2be142745e3d2313e25b71e4340da97331ccebaf
Ashwin Krishna K <ashwinkrishna7151@gmail.com>
2024-07-08 13:22:10 +0530

    Open AI describe Image example added.
    Signed-off-by: Ashwin Krishna K <ashwinkrishna7151@gmail.com>

--- src/main/java/com/learning/springaitest/controller/ImageGenerationController.java
+++ src/main/java/com/learning/springaitest/controller/ImageGenerationController.java
@@ -4,9 +4,14 @@ import com.learning.springaitest.model.Question;
 import com.learning.springaitest.service.ImageGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class ImageGenerationController {
@@ -17,5 +22,11 @@ public class ImageGenerationController {
     @PostMapping(value = "/image", produces = MediaType.IMAGE_PNG_VALUE)
     public byte[] generateImage(@RequestBody Question question){
         return imageGenerationService.generateImage(question);
+    }
+
+    @PostMapping(value = "/image/describe", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String describeImage(@Validated @RequestParam("file") MultipartFile file,
+                                @RequestParam("prompt") String prompt) throws IOException {
+        return  imageGenerationService.getDescription(file, prompt);
     }
 }
--- src/main/java/com/learning/springaitest/service/ImageGenerationService.java
+++ src/main/java/com/learning/springaitest/service/ImageGenerationService.java
@@ -1,22 +1,36 @@
 package com.learning.springaitest.service;
 
 import com.learning.springaitest.model.Question;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Media;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImageGeneration;
 import org.springframework.ai.image.ImageOptions;
 import org.springframework.ai.image.ImageOptionsBuilder;
 import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 
 @Service
 public class ImageGenerationService {
 
     @Autowired
     private OpenAiImageModel imageModel;
+
+    @Autowired
+    private ChatClient chatClient;
 
     public byte[] generateImage(Question question){
         var options = OpenAiImageOptions.builder()
@@ -29,5 +43,16 @@ public class ImageGenerationService {
         ImagePrompt imagePrompt = new ImagePrompt(question.question(),options);
         var imageResponse = imageModel.call(imagePrompt);
         return Base64.getDecoder().decode(imageResponse.getResult().getOutput().getB64Json());
+    }
+
+    public String getDescription(MultipartFile file,String prompt) throws IOException{
+        var options = OpenAiChatOptions.builder()
+                .withModel(OpenAiApi.ChatModel.GPT_4_VISION_PREVIEW)
+                .build();
+        var userMessage = new UserMessage(prompt,
+                        List.of(new Media(MimeTypeUtils.IMAGE_JPEG,file.getResource())));
+        return chatClient.prompt(new Prompt(userMessage))
+                .call()
+                .content();
     }
 }
